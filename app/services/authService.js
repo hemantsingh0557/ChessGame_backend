e1f6451240727ee5cd4557c67d5426409f9e6dfa
hemantsingh0557 <hemantsingh0557@gmail.com>
2024-09-26 16:48:26 +0530

    update user routes and file upload

--- app/services/authService.js
+++ app/services/authService.js
@@ -4,7 +4,7 @@ const { API_AUTH_KEY } = require('../../config');
 const CONFIG = require('../../config');
 const { decryptJwt } = require('../utils/utils');
 const { createErrorResponse } = require('../helpers');
-const { conversationRoomModel, userModel } = require('../models');
+const { UserModel } = require('../models');
 const { MESSAGES, ERROR_TYPES } = require('../utils/constants');
 const CONSTANTS = require('../utils/constants');
 const commonFunctions = require('../utils/utils');
@@ -29,8 +29,9 @@ authService.userValidate = () => async (req, res, next) => {
         if (!decodedToken || !decodedToken.userId) {
             return res.status(401).json(createErrorResponse(MESSAGES.UNAUTHORIZED, ERROR_TYPES.UNAUTHORIZED));
         }
+        console.log("nonono " ,  !decodedToken , !decodedToken.userId ) ;
         const { userId } = decodedToken;
-        const user = await userModel.findOne({ where: { id: userId } });
+        const user = await UserModel.findOne({ where: { id: userId } });
         if (!user) {
             return res.status(401).json(createErrorResponse(MESSAGES.UNAUTHORIZED, ERROR_TYPES.UNAUTHORIZED));
         }
--- app/services/userService.js
+++ app/services/userService.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { userModel, UserModel } = require('../models');
+const { UserModel } = require('../models');
 const { NORMAL_PROJECTION } = require('../utils/constants');
 
 
@@ -9,9 +9,9 @@ const { NORMAL_PROJECTION } = require('../utils/constants');
 const userService = {};
 
 
-userService.findOne = async (criteria) => await userModel.findOne({ where: criteria });
+userService.findOne = async (criteria) => await UserModel.findOne({ where: criteria });
 
-userService.create = async (payload) => await userModel.create(payload);
+userService.create = async (payload) => await UserModel.create(payload);
 
 userService.updateUser = async (condition, updates) => {
     const [updated] = await UserModel.update(updates, { where: condition });
