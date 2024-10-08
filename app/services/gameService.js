4197fe604564844c6d73fb1be63450ee5310d47a
hemantsingh0557 <hemantsingh0557@gmail.com>
2024-10-08 17:46:33 +0530

    add service to get all the games history

--- app/services/gameService.js
+++ app/services/gameService.js
@@ -9,5 +9,8 @@ gameService.createGameRoom = async(payload) => await GameModel.create(payload)  
 gameService.checkIfRoomExists = async(payload) => await GameModel.findOne(payload)  ;
 
 
+gameService.getAllGamesFromDB = async(payload) => await GameModel.findAll(payload)  ;
+
+
 
 module.exports = gameService ;
