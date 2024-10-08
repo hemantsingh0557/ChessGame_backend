4ca66a263dedc3b272db9cfe59fb05f124fa14ee
hemantsingh0557 <hemantsingh0557@gmail.com>
2024-10-08 13:45:14 +0530

    write basic controller for the get moves history

--- app/controllers/gameController.js
+++ app/controllers/gameController.js
@@ -3,7 +3,6 @@
 const { Chess } = require("chess.js");
 const { createSuccessResponse, createErrorResponse } = require("../helpers");
 const { userService, gameService, gameStateService } = require("../services");
-const { MESSAGES, ERROR_TYPES } = require("../utils/constants");
 const { Op, where } = require('sequelize');
 const CONSTANTS = require("../utils/constants");
 
@@ -12,58 +11,25 @@ const CONSTANTS = require("../utils/constants");
 const gameController = {};
 
 
-// gameController.startGame = async (payload) => {
-//     const { user } = payload;
-//     const userId = user.id;
-//     await userService.updateUser({ id: userId }, { isLookingForGame: true });
-//     const matchDuration = CONSTANTS.MATCH_DURATION;  
-//     const checkInterval = CONSTANTS.CHECK_INTERVAL ;  
-//     const endTime = Date.now() + matchDuration;
-//     return new Promise((resolve, reject) => {
-//         const checkForMatch = async () => {
-//             if (Date.now() >= endTime) {
-//                 return resolve(createErrorResponse(MESSAGES.NO_MATCH_FOUND, ERROR_TYPES.BAD_REQUEST));
-//             }
-//             const allUsersLookingForGame = await userService.allUsersLookingForGameFromDB({
-//                 where: {
-//                     isLookingForGame: true,
-//                     id: {
-//                         [Op.ne]: userId 
-//                     }
-//                 }
-//             });
-//             if (allUsersLookingForGame.length > 0) {
-//                 const opponentPlayer = allUsersLookingForGame[0] ;
-//                 await userService.updateUser( 
-//                     { [Op.or]: [{ id: userId }, { id: opponentPlayer.id }] },
-//                     { isLookingForGame: false }
-//                 );
-//                 const gameRoom = await gameService.createGameRoom({
-//                     userId1 : userId ,
-//                     userId2 : opponentPlayer.id ,
-//                 }) ;
-//                 const initialBoardState = new Chess().fen(); 
-//                 const initialGameState = await gameStateService.createGameState(
-//                     { 
-//                         gameRoomId : gameRoom.id  ,
-//                         boardState : initialBoardState 
-//                     }
-//                 ) ;
-//                 const responseObejct = { 
-//                     gameRoomId : gameRoom.id , 
-//                     opponentPlayerId: opponentPlayer.id ,
-//                     boardState : initialGameState.boardState,
-//                     currentTurn : initialBoardState.currentTurn ,
-//                 }
-//                 return resolve(createSuccessResponse(MESSAGES.MATCH_FOUND_SUCCESSFULLY, responseObejct ));
-//             }
-//             else {
-//                 setTimeout(checkForMatch, checkInterval);
-//             }
-//         };
-//         checkForMatch(); 
-//     });
-// };
+gameController.getMovesHistory = async (payload) => {
+    const { user , gameRoomId } = payload;
+    console.log( user , gameRoomId ) ;
+    const roomExists = await gameService.checkIfRoomExists({ where : { id : gameRoomId }});  
+    if (!roomExists) {
+        return createErrorResponse(CONSTANTS.MESSAGES.GAME_ROOM_NOT_EXISTS , CONSTANTS.ERROR_TYPES.DATA_NOT_FOUND)
+    }
+    console.log( roomExists )
+    const gameStates = await gameStateService.getAllGameState({
+        where: { gameRoomId },
+        order: [['createdAt', 'ASC']]  ,
+        attributes : ["currentMove"]
+    });
+    if (!gameStates || !gameStates.length ) {
+        return createErrorResponse(CONSTANTS.MESSAGES.GAME_STATE_NOT_FOUND , CONSTANTS.ERROR_TYPES.DATA_NOT_FOUND)
+    }
+    console.log( gameStates ) ;
+    return createSuccessResponse(CONSTANTS.MESSAGES.SUCCESS , gameStates )
+};
 
 
 
