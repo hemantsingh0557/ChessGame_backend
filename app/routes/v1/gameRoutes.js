9b16c8acadaf33041e35fd90f4b53240e4cf890e
hemantsingh0557 <hemantsingh0557@gmail.com>
2024-10-08 17:24:43 +0530

    add routes for get current game state

--- app/routes/v1/gameRoutes.js
+++ app/routes/v1/gameRoutes.js
@@ -6,6 +6,23 @@ const { Joi } = require("../../utils/joiUtils");
 module.exports = [
     {
         method : "GET" ,
+        path : "/getGameCurrentState/:gameRoomId",
+        joiSchemaForSwagger : {
+            group: 'Game',
+            description: 'Route to get the current state of the game ',
+            model: 'getGameCurrentState',
+            headers: {
+                authorization: Joi.string().required()
+            },
+            params : {
+                gameRoomId: Joi.string().uuid().required(),
+            } ,  
+        } ,
+        auth : true ,
+        handler: gameController.getGameCurrentState ,
+    } ,
+    {
+        method : "GET" ,
         path : "/getMovesHistory/:gameRoomId",
         joiSchemaForSwagger : {
             group: 'Game',
@@ -24,5 +41,23 @@ module.exports = [
         } ,
         auth : true ,
         handler: gameController.getMovesHistory ,
-    }
+    } ,
+    // {
+    //     method : "GET" ,
+    //     path : "/getUserAllGameHistory/",
+    //     joiSchemaForSwagger : {
+    //         group: 'Game',
+    //         description: 'Route to get all the games of the user ',
+    //         model: 'getUserAllGameHistory',
+    //         headers: {
+    //             authorization: Joi.string().required()
+    //         },
+    //         query :{
+    //             skip : Joi.number().default(0) ,
+    //             limit : Joi.number().default(20) ,
+    //         }    
+    //     } ,
+    //     auth : true ,
+    //     handler: gameController.getUserAllGameHistory ,
+    // }
 ]
