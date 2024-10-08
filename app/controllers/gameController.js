d0e3453ac31e66b87b201c0acb72a9229ad4744a
hemantsingh0557 <hemantsingh0557@gmail.com>
2024-10-08 17:25:03 +0530

    add controller for get current game state

--- app/controllers/gameController.js
+++ app/controllers/gameController.js
@@ -9,6 +9,42 @@ const { sequelize } = require("../startup/db_mySql");
 
 const gameController = {};
 
+gameController.getGameCurrentState = async (payload) => {
+    const { user , gameRoomId } = payload;
+    const userId = user.id ;
+    const roomExists = await gameService.checkIfRoomExists({ where: { id: gameRoomId } });
+    if (!roomExists) {
+        return createErrorResponse(CONSTANTS.MESSAGES.GAME_ROOM_NOT_EXISTS, CONSTANTS.ERROR_TYPES.DATA_NOT_FOUND);
+    }
+    const playerOneColor = roomExists.userId1 == userId ? roomExists.playerOneColor : roomExists.playerTwoColor ;
+    const opponentId = roomExists.userId1 == userId ? roomExists.userId2 : roomExists.userId1 ;
+    const opponent = await userService.findOne({ id : opponentId  }) ;
+    const gameState = await gameStateService.getCurrentGameState({
+        where: { gameRoomId },
+        order: [['createdAt', 'DESC']]  
+    });
+    if (!gameState) {
+        return createErrorResponse(CONSTANTS.MESSAGES.GAME_STATE_NOT_FOUND, CONSTANTS.ERROR_TYPES.DATA_NOT_FOUND);
+    }
+    const boardState = gameState.boardState
+    const chess = new Chess();
+    chess.load(boardState);
+    const currentTurn = chess.turn();
+    const responseObject = {
+        gameRoomId,
+        opponentDetails: {
+            id : opponent.id ,
+            name : opponent.name ,
+            username : opponent.username ,
+            imageUrl : opponent.imageUrl
+        },
+        boardState: gameState.boardState ,
+        turn : currentTurn ,
+        orientation : playerOneColor ,
+    };
+    return createSuccessResponse(CONSTANTS.MESSAGES.GAME_STATE_FOUND, responseObject );
+}
+
 gameController.getMovesHistory = async (payload) => {
     const { user, gameRoomId , skip , limit } = payload;
     const roomExists = await gameService.checkIfRoomExists({ where: { id: gameRoomId } });
@@ -28,7 +64,7 @@ gameController.getMovesHistory = async (payload) => {
         limit: limit   
     });    
     if (!gameMovesHistory ) {
-        return createErrorResponse(CONSTANTS.MESSAGES.GAME_STATE_NOT_FOUND, CONSTANTS.ERROR_TYPES.DATA_NOT_FOUND);
+        return createErrorResponse(CONSTANTS.MESSAGES.GAME_MOVES_NOT_FOUND, CONSTANTS.ERROR_TYPES.DATA_NOT_FOUND);
     }
     const whiteMoves = gameMovesHistory.filter(data => data.currentTurn === 'w');
     const blackMoves = gameMovesHistory.filter(data => data.currentTurn === 'b');
@@ -45,5 +81,17 @@ gameController.getMovesHistory = async (payload) => {
     });
     return createSuccessResponse(CONSTANTS.MESSAGES.SUCCESS, combinedMoves );
 };
+
+
+
+// gameService.getUserAllGameHistory = async(payload) => {
+//     const {user } = payload ;
+//     const getAllGames = await gameService.getAllGamesFromDB(
+//         { where: { id: gameRoomId } }
+//     );
+//     if (!roomExists) {
+//         return createErrorResponse(CONSTANTS.MESSAGES.GAME_ROOM_NOT_EXISTS, CONSTANTS.ERROR_TYPES.DATA_NOT_FOUND);
+//     }
+// }
 
 module.exports = gameController;
