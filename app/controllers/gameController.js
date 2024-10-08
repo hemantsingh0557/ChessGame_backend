84af397d4f5f2677ed9dcd18bbf223d9752d34f9
hemantsingh0557 <hemantsingh0557@gmail.com>
2024-10-08 15:35:56 +0530

    complete the logic and update response

--- app/controllers/gameController.js
+++ app/controllers/gameController.js
@@ -10,7 +10,7 @@ const { sequelize } = require("../startup/db_mySql");
 const gameController = {};
 
 gameController.getMovesHistory = async (payload) => {
-    const { user, gameRoomId } = payload;
+    const { user, gameRoomId , skip , limit } = payload;
     const roomExists = await gameService.checkIfRoomExists({ where: { id: gameRoomId } });
     if (!roomExists) {
         return createErrorResponse(CONSTANTS.MESSAGES.GAME_ROOM_NOT_EXISTS, CONSTANTS.ERROR_TYPES.DATA_NOT_FOUND);
@@ -18,26 +18,32 @@ gameController.getMovesHistory = async (payload) => {
     const gameMovesHistory = await gameStateService.getAllGameState({
         where: { gameRoomId },
         attributes: [
-            'currentTurn' ,
+            'currentTurn',
             'currentMove',
             'piece',
             'promotedPiece',
         ],
-        order: [['createdAt', 'ASC']], 
-        offset: 1, 
-    });
-    if (!gameMovesHistory || !gameMovesHistory.length) {
+        order: [['createdAt', 'ASC']],  
+        offset: skip + 1,   
+        limit: limit   
+    });    
+    if (!gameMovesHistory ) {
         return createErrorResponse(CONSTANTS.MESSAGES.GAME_STATE_NOT_FOUND, CONSTANTS.ERROR_TYPES.DATA_NOT_FOUND);
     }
-    const whiteMoves = gameMovesHistory.filter(data => data.currentTurn === 'w')  ;
-    const blackMoves = gameMovesHistory.filter(data => data.currentTurn === 'b') ;
-    const responseObject = {
-        whiteMoves ,
-        blackMoves
-    } ;
-    console.log( whiteMoves  ) ;
-    console.log( blackMoves  ) ;
-    return createSuccessResponse(CONSTANTS.MESSAGES.SUCCESS, responseObject );
+    const whiteMoves = gameMovesHistory.filter(data => data.currentTurn === 'w');
+    const blackMoves = gameMovesHistory.filter(data => data.currentTurn === 'b');
+    const combinedMoves = whiteMoves.map((whiteMove, index) => {
+        const blackMove = blackMoves[index] || {}; 
+        return {
+            whiteMove: whiteMove.currentMove?.substr(3),
+            whitePiece: whiteMove.piece,
+            whitePromotedPiece: whiteMove.promotedPiece,
+            blackMove: blackMove.currentMove?.substr(3) || null, 
+            blackPiece: blackMove.piece || null,
+            blackPromotedPiece: blackMove.promotedPiece || null,
+        };
+    });
+    return createSuccessResponse(CONSTANTS.MESSAGES.SUCCESS, combinedMoves );
 };
 
 module.exports = gameController;
